22bc209310a2ea90e58469900c1770760f10c860
Pablo Anastasio <pabloanastasio4326@gmail.com>
2022-07-15 17:05:59 +0200

    First commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+
+export const AddCategory = ({onNewCategory}) => {
+  
+    const [inputValue, setinputValue] = useState('Interstellar')
+  
+    const onInputChange = ({target}) => {
+        setinputValue(target.value)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const inputValueTrim = inputValue.trim();
+        if(inputValueTrim.length<=1) return;
+        //setCategories(categories => [inputValue, ...categories]);
+        onNewCategory(inputValueTrim);
+        setinputValue('');
+    }
+
+        return (
+           <form onSubmit={onSubmit}> 
+            <input
+                type="text"
+                placeholder="Buscar Gifs"
+                value={inputValue}
+                onChange={onInputChange}
+            />
+           </form> 
+        )
+}
